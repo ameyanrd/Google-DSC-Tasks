030b8111c52b0d43831824e3adc8df9fd6dbc145
Ameya Deshpande <ameyanrd@outlook.com>
2019-08-21 20:31:46 +0530

    Dialogflow SmallTask implemented

--- Chat_bot/app/src/main/java/com/example/chat_bot/MainActivity.java
+++ Chat_bot/app/src/main/java/com/example/chat_bot/MainActivity.java
@@ -1,19 +1,18 @@
-    package com.example.chat_bot;
+package com.example.chat_bot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,7 +22,19 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class MainActivity extends AppCompatActivity {
+import ai.api.AIDataService;
+
+import ai.api.AIListener;
+import ai.api.AIServiceException;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIRequest;
+import ai.api.model.AIResponse;
+import ai.api.model.Result;
+
+
+public class MainActivity extends AppCompatActivity implements AIListener{
 
     private EditText etMessage;
     private RecyclerView recyclerView;
@@ -32,17 +43,27 @@ import java.util.List;
     private List<ChatMessage> chatMessages;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference myref;
+    private final static String ACCESS_TOKEN="9b345b5caead4dd08c5ae1626f14dd78 ";
+    private AIService aiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final AIConfiguration config = new AIConfiguration(ACCESS_TOKEN, AIConfiguration.SupportedLanguages.English,
+                AIConfiguration.RecognitionEngine.System);
+        aiService = AIService.getService(this, config);
+        aiService.setListener(this);
+
+        final AIDataService aiDataService = new AIDataService(config);
+        final AIRequest aiRequest = new AIRequest();
+
         recyclerView = findViewById(R.id.recyclerView);
         etMessage = findViewById(R.id.etMessage);
         addBtn = findViewById(R.id.addBtn);
         chatMessages = new ArrayList<>();
-        
+
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setStackFromEnd(true);
@@ -55,7 +76,7 @@ import java.util.List;
         myref.child("chat").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot postSnapShot: dataSnapshot.getChildren()){
+                for (DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
                     ChatMessage chatMessage = postSnapShot.getValue(ChatMessage.class);
                     //Log.e("Get Data",chatMessage.getMessage());
                     chatMessages.add(chatMessage);
@@ -66,7 +87,7 @@ import java.util.List;
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e("The read failed: " , databaseError.getMessage());
+                Log.e("The read failed: ", databaseError.getMessage());
             }
         });
 
@@ -76,22 +97,76 @@ import java.util.List;
             public void onClick(View view) {
 
                 String message = etMessage.getText().toString().trim();
+                final ChatMessage chatMessage = new ChatMessage(message, "user");
+                myref.child("chat").push().setValue(chatMessage);
+                chatMessages.add(chatMessage);
+                adapter = new MyAdapter(MainActivity.this, chatMessages);
+                recyclerView.setAdapter(adapter);
+                aiRequest.setQuery(message);
+                new AsyncTask<AIRequest,Void, AIResponse>() {
 
-//                if(!message.equals("")){
-                    //Log.i("signal","Add Btn pressed!");
-                    ChatMessage chatMessage = new ChatMessage(message, "user");
-                    myref.child("chat").push().setValue(chatMessage);
-                    chatMessages.add(chatMessage);
-                    adapter = new MyAdapter(MainActivity.this, chatMessages);
-                    recyclerView.setAdapter(adapter);
-//                }
+                    @Override
+                    protected AIResponse doInBackground(AIRequest... aiRequests) {
+                        final AIRequest request = aiRequests[0];
+                        try {
+                            final AIResponse response = aiDataService.request(aiRequest);
+                            return response;
+                        } catch (AIServiceException e) {
+                        }
+                        return null;
+                    }
+
+                    @Override
+                    protected void onPostExecute(AIResponse response) {
+                        if (response != null) {
+
+                            Result result = response.getResult();
+                            String reply = result.getFulfillment().getSpeech();
+                            ChatMessage chatMessage = new ChatMessage(reply, "bot");
+                            myref.child("chat").push().setValue(chatMessage);
+                            chatMessages.add(chatMessage);
+                            adapter = new MyAdapter(MainActivity.this, chatMessages);
+                            recyclerView.setAdapter(adapter);
+                        }
+                    }
+                }.execute(aiRequest);
+
                 etMessage.setText("");
-                Log.i("size",String.valueOf(chatMessages.size()));
-                Log.i("message",chatMessages.get(0).getMessage());
+                Log.i("size", String.valueOf(chatMessages.size()));
+                Log.i("message", chatMessages.get(0).getMessage());
 
             }
         });
 
+    }
+
+    @Override
+    public void onResult(AIResponse result) {
+
+    }
+
+    @Override
+    public void onError(AIError error) {
+        Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onAudioLevel(float level) {
+
+    }
+
+    @Override
+    public void onListeningStarted() {
+
+    }
+
+    @Override
+    public void onListeningCanceled() {
+
+    }
+
+    @Override
+    public void onListeningFinished() {
 
     }
 }
